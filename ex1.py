ea0696d054ea121d0a9d408ccc314ffae6f990a9
azapien22 <azapien@hotmail.com>
2023-04-27 18:32:35 -0500

    Exercise 1 & Study Drill

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# Exercise 1. A good first program
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("THis is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
---
+++ ex1studydrill.py
@@ -0,0 +1,15 @@
+# Exercise 1. A good first program
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+#Study Drill
+#Print another Line
+print("Another line.")
+#Print only one of the lines
+print("Only one of the lines.")
+# An Octothorpe comments out the line. Used for reading by a programmer.
