af3a32c706b4d5f49ec4951c0044309714a4e8a0
azapien22 <azapien@hotmail.com>
2023-04-27 23:46:59 -0500

    Study Drill

---
+++ ex3sd.py
@@ -0,0 +1,75 @@
+# This line of code prints the string "I will now count my chickens:"
+print("I will now count my chickens:")
+
+# This line of code prints the string "Hens" followed by the result of the arithmetic expression (25 + 30 / 6)
+print("Hens", 25 + 30 / 6)
+
+# This line of code prints the string "Roosters" followed by the result of the arithmetic expression (100 - 25 * 3 / 6)
+print("Roosters", 100 - 25 * 3 / 6)
+
+# This line of code prints the string "Now I will count the eggs:"
+print("Now I will count the eggs:")
+
+# This line of code prints the result of the arithmetic expression (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# This line of code prints the string "Is it true that 3 + 2 < 5 - 7?"
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# This line of code prints the result of the comparison expression (3 + 2 < 5 - 7)
+print(3 + 2 < 5 - 7)
+
+# This line of code prints the string "What is 3 + 2?" followed by the result of the arithmetic expression (3 + 2)
+print("What is 3 + 2?", 3 + 2)
+
+# This line of code prints the string "What is 5 - 7?" followed by the result of the arithmetic expression (5 - 7)
+print("What is 5 - 7?", 5 - 7)
+
+# This line of code prints the string "Oh, that's why it's false."
+print("Oh, that's why it's false.")
+
+# This line of code prints the string "How about some more."
+print("How about some more.")
+
+# This line of code prints the string "Is it greater?" followed by the result of the comparison expression (5 > -2)
+print("Is it greater?", 5 > -2)
+
+# This line of code prints the string "Is it greater or equal?" followed by the result of the comparison expression (5 >= -2)
+print("Is it greater or equal?", 5 >= -2)
+
+# This line of code prints the string "Is it less or equal?" followed by the result of the comparison expression (5 <= -2)
+print("Is is less or equal?", 5 <= -2)
+
+# Code Rendered with floating point numbers:
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 *3.0 / 6.0)
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 +4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 -7?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's false.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is is less or equal?", 5.0 <= -2.0)
+
+# Calculate the square root of pie using python arithmetic:
+# To calculate the square root of pi using Python arithmetic, we first need to import the math module which provides 
+# several mathematical functions including the square root function. Then we can simply use the math.sqrt() function 
+# to calculate the square root of pi as follows:
+import math
+sqrt_pi = math.sqrt(math.pi)
+print(sqrt_pi)
