0ac6343f243beda5ad6253bb91d6e79f81775bd7
azapien22 <azapien@hotmail.com>
2023-04-29 15:46:23 -0500

    Printing, Printing, Printing

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# Exercise 9. Printing, Printing, printing
+# Here's some new strange stuff, remember type it exactly
+
+# Assigning a string with space-separated days of the week to a variable called days.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# Assigning a string with newline-separated months to a variable called months.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Printing a string literal followed by the value of the variable 'days'.
+print("Here are the days:", days)
+
+# Printing a string literal followed by the value of the variable 'months'.
+print("Here are the months:", months)
+
+# Printing a multi-line string literal, using triple quotes, which contains text across multiple lines. The string is not assigned to a variable.
+print(""" There's something going on here. With the three double-quotes. We'll be able to type as much as we like. Even 4 lines if we want, or 5, or 6.""")
