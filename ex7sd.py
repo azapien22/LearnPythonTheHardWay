cffeb3e1af408f45b25a01efa99d90cb0e1e4316
azapien22 <azapien@hotmail.com>
2023-04-29 11:10:01 -0500

    Study Drill

---
+++ ex7sd.py
@@ -0,0 +1,32 @@
+# This line prints the string "Mary had a little lamb"
+print("Mary had a little lamb")
+
+# This line uses the .format() method to insert the string 'snow' into the string and prints it
+print("It's fleece was white as {}.".format('snow'))
+
+# This line prints the string "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+
+# This line prints the string "." ten times using string repetition
+print("." * 10) # What'd that do?
+
+# These lines assign string values to variables
+end1 ="C"
+end2 ="h"
+end3 ="e"
+end4 ="e"
+end5 ="s"
+end6 ="e"
+end7 ="B"
+end8 ="u"
+end9 ="r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# This line concatenates the values of the first six variables and prints them with a space as separator
+# The end parameter is set to ' ' so that the next print statement will continue on the same line
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+
+# This line concatenates the values of the last six variables and prints them
+print(end7 + end8 +end9 + end10 + end11 +end12)
