066e5355fb787b387a9c668470d7d673fe2d6c63
azapien22 <azapien@hotmail.com>
2023-05-01 09:21:15 -0500

    Ex. 12 Prompting People

---
+++ ex12.py
@@ -0,0 +1,13 @@
+# Exercise 12. Prompting People
+
+# Ask the user for their age and store the input in the `age` variable
+age = input("How old are you? ")
+
+# Ask the user for their height and store the input in the `height` variable
+height = input("How tall are you? ")
+
+# Ask the user for their weight and store the input in the `weight` variable
+weight = input("How much do you weigh? ")
+
+# Use an f-string to print out a message that includes the user's input
+print(f"So you're {age} old, {height} tall, and {weight} heavy.")
