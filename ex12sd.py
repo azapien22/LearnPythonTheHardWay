930bb9a0132ee7d614fa11ae06f1125c5b77eca4
azapien22 <azapien@hotmail.com>
2023-05-01 10:10:05 -0500

    Study Drill

---
+++ ex12sd.py
@@ -0,0 +1,22 @@
+# Look online for what pydoc is and what the command does.
+
+"""Pydoc is a documentation generator and online help system that comes with Python. The pydoc command is used to access Python 
+
+documentation in various formats, including HTML, plain text, and man pages. When you run the pydoc command in a terminal, it launches 
+
+a web server and opens a web browser to display the Python documentation. By default, the documentation displayed is for the Python version
+
+ that you are currently using. The pydoc command can be used to display the documentation for modules, classes, functions, and other objects 
+ 
+ in the Python standard library, as well as for any user-defined modules and packages that you have installed.
+ 
+ For example, to display the documentation for the built-in os module, you can run the following command in a terminal: """
+
+pydoc os
+
+pydoc input
+
+
+"""This will display the documentation for the os module in your web browser. You can also use the -g option to launch a graphical interface 
+
+for browsing the Python documentation, or the -w option to write the documentation to an HTML file."""
