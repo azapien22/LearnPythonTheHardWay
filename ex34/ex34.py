66f5dde5abe1f5d5d4bc8a7429f8fea77e8116e3
azapien22 <azapien@hotmail.com>
2023-07-06 21:50:32 -0500

    Exercise 34. Acessing Elements of Lists

---
+++ ex34/ex34.py
@@ -0,0 +1,34 @@
+# Exercise 34 Accessing Elements of Lists
+
+# A list of strings representing different animals
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# This line is grabbing the first animal from the list, which is 'bear'. 
+# Remember, list indices in Python start at 0, so the first element is at index 0.
+animal_at_1 = animals[0]
+
+# This line is grabbing the third animal from the list, which is 'peacock'.
+third_animal = animals[2]
+
+# This line is also grabbing the first animal from the list, which is 'bear'.
+first_animal = animals[0]
+
+# This line is grabbing the third animal from the list, which is 'peacock'.
+animal_at_3 = animals[2]
+
+# This line is grabbing the fifth animal from the list, which is 'whale'.
+fifth_animal = animals[4]
+
+# This line is grabbing the second animal from the list, which is 'python3.6'.
+animal_at_2 = animals[1]
+
+# This line is grabbing the sixth animal from the list, which is 'platypus'.
+sixth_animal = animals[5]
+
+# This line is grabbing the fourth animal from the list, which is 'kangaroo'.
+animal_at_4 = animals[3]
+
+# Note that the variable names (e.g., animal_at_1, third_animal) can be a bit misleading because they don't align with
+# Python's 0-based indexing. For instance, animal_at_1 is actually referring to the first animal ('bear') which is at
+# index 0, not the animal at index 1 ('python3.6'). Similarly, third_animal is referring to the animal at index 2, which
+# is the third animal in the list.
